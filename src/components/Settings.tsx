1cc0ebba744bba3d46766559c1c7bb8710ff7420
Ivan Violentov <violentoff@gmail.com>
2021-10-03 20:39:29 +0300

    Scss cleanup

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 import { Tabs, Language, DateRange, SpokenLanguage } from '.'
 
-const Settings = () => (
-  <div className='Box-header d-flex flex-items-center flex-justify-between'>
-    <Tabs />
-    <div className='d-flex flex-items-center flex-justify-end mt-0 table-list-header-toggle ml-2'>
-      <SpokenLanguage />
-      <Language />
-      <DateRange />
+const Settings = () => {
+  const current = useLocation().pathname.replace('/', '')
+
+  return (
+    <div className='Box-header d-flex flex-items-center flex-justify-between'>
+      <Tabs />
+      <div className='d-flex flex-items-center flex-justify-end mt-0 table-list-header-toggle ml-2'>
+        {current === 'repositories' && <SpokenLanguage />}
+        <Language />
+        <DateRange />
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Settings
