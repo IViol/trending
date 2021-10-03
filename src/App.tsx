7779dc36a104de659e94ce0560de20b7a232c21e
Ivan Violentov <violentoff@gmail.com>
2021-10-03 15:13:27 +0300

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
 
-export default App;
+import { Developers, Repositories } from './pages'
+
+import './styles/index.scss'
+
+const App = () => (
+  <Router>
+      <Switch>
+        <Redirect exact from="/" to="repositories" />
+        <Route path="/repositories">
+          <Repositories />
+        </Route>
+        <Route path="/developers">
+          <Developers />
+        </Route>
+      </Switch>
+  </Router>
+)
+
+export default App
